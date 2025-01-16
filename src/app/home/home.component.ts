07e7c0e625f2c5a90259da80af0ba307910c4fac
Nilakshi1223 <nmiyurangika1223@gmail.com>
2025-01-16 22:08:53 +0530

    Describe updates made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  logo : string = 'https://clipart-library.com/2023/61-612180_kind-face-clip-art-download-fox-emoji-png.png';
 
 }
